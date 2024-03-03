cdb8881db6016af09867c2ef3515cb6a10cc9af8
branchtree <branchtree97@outlook.com>
2024-03-03 18:36:08 +0800

    文件上传OSS

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -15,7 +15,6 @@ public interface DishFlavorMapper {
      * 批量插入口味数据
      * @param flavors
      */
-    @AutoFill(value = OperationType.INSERT)
     void insertBatch(List<DishFlavor> flavors);
 
     /**
