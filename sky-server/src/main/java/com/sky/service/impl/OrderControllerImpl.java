e2d069a96645f6ec8e2f32f1a290980bfa89a979
branchtree <branchtree97@outlook.com>
2024-03-14 15:46:06 +0800

    用户下单代码开发，未完成微信支付

---
+++ sky-server/src/main/java/com/sky/service/impl/OrderControllerImpl.java
@@ -0,0 +1,99 @@
+package com.sky.service.impl;
+
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.AddressBook;
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.mapper.OrderDetailMapper;
+import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapping;
+import com.sky.service.OrderControllerService;
+import com.sky.vo.OrderSubmitVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderControllerImpl implements OrderControllerService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
+
+    @Autowired
+    private ShoppingCartMapping shoppingCartMapping;
+
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
+
+        //抛出异常（地址簿为空，购物车为空）
+        AddressBook addressBook=addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
+        if(addressBook==null){
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+        //查询当前用户的购物车数据
+        ShoppingCart shoppingCart=new ShoppingCart();
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+        List<ShoppingCart> list = shoppingCartMapping.list(shoppingCart);
+        if(list==null||list.size()==0){
+            throw new AddressBookBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+
+        //向订单表插入数据
+        Orders order = new Orders();
+
+        BeanUtils.copyProperties(ordersSubmitDTO,order);
+        order.setPhone(addressBook.getPhone());
+        order.setAddress(addressBook.getDetail());
+        order.setConsignee(addressBook.getConsignee());
+        order.setNumber(String.valueOf(System.currentTimeMillis()));
+        order.setUserId(BaseContext.getCurrentId());
+        order.setStatus(Orders.PENDING_PAYMENT);
+        order.setPayStatus(Orders.UN_PAID);
+        order.setOrderTime(LocalDateTime.now());
+
+        orderMapper.insert(order);
+        //订单明细数据
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        for (ShoppingCart cart : list) {
+            OrderDetail orderDetail = new OrderDetail();
+            BeanUtils.copyProperties(cart, orderDetail);
+            orderDetail.setOrderId(order.getId());
+            orderDetailList.add(orderDetail);
+        }
+
+        //向明细表插入n条数据
+        orderDetailMapper.insertBatch(orderDetailList);
+
+        //清理购物车中的数据
+        shoppingCartMapping.cleanShoppingCart(BaseContext.getCurrentId());
+
+        //封装返回结果
+        OrderSubmitVO orderSubmitVO = OrderSubmitVO.builder()
+                .id(order.getId())
+                .orderNumber(order.getNumber())
+                .orderAmount(order.getAmount())
+                .orderTime(order.getOrderTime())
+                .build();
+
+        return orderSubmitVO;
+    }
+}
