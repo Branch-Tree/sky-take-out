1cf266672aebf9a42092755c37ca3c1f6f8b49ff
branchtree <branchtree97@outlook.com>
2024-03-17 19:17:21 +0800

    管理端查询订单详情代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -1,17 +1,22 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 订单管理
@@ -46,5 +51,17 @@ public class OrderController {
     public Result<OrderStatisticsVO> orderStatistics(){
         OrderStatisticsVO orderStatisticsVO=orderService.statistics();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 查询订单详细
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("查询订单详细")
+    public Result<OrderVO> orderDetails(@PathVariable Long id){
+        OrderVO orderVO=orderService.getOrderDetails(id);
+        return Result.success(orderVO);
     }
 }
